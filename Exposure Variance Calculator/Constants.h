337bcb00efedf91eba9fa24cd44d3d51c60f0b46
Thomas <91362412+BEG5210@users.noreply.github.com>
2026-05-03 20:10:18 +1000

    Update repository URL in Constants.h #14 solved

--- Exposure Variance Calculator/Constants.h
+++ Exposure Variance Calculator/Constants.h
@@ -3,7 +3,7 @@
 #include <string>
 
 // Update once repURL changes [duh]
-const std::string repoUrl = "https://github.com/BEG5210/Exposure-Variance-Calculator.git";
+const std::string repoUrl = "https://github.com/BEG5210/Exposure-Value-Calculator.git";
 
 // Default camrea settings values. To be updated into the camera settings class.
 const float defaultShutterSpeed = 1.0f;
